a3511102ca01472d0b33cf6a758eaa3913729096
DMProductionn <goluzin.dima008@mail.ru>
2025-06-30 16:42:45 +0500

    add

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  // Если используете basePath (для GitHub Pages)
-  basePath: process.env.NODE_ENV === 'production' ? '/DM_site' : '',
-  // Опционально: настройки для изображений
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
