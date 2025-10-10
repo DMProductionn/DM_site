94a8aa68ac76db401101b6837868752214ced2a9
DMProductionn <goluzin.dima008@mail.ru>
2025-10-10 09:50:01 +0500

    l

--- src/widgets/services/services.constants.ts
+++ src/widgets/services/services.constants.ts
@@ -1,7 +1,7 @@
 export const SERVICES = [
   {
     description:
-      'lorem ipsum dolor sit amet consectetur dima adipisicing elit. Quisquam, quae faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.',
+      'llorem ipsum dolor sit amet consectetur dima adipisicing elit. Quisquam, quae faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.',
     clients: ['Lorem ipsum', 'Lorem ipsum'],
   },
   {
